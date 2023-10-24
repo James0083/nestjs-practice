323c6cdc348a925a50890ebc59fb43023110b3a6
You JaeWon <71857831+James0083@users.noreply.github.com>
2023-10-24 16:29:04 +0900

    Ch5 Modularization

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,20 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ServiceB } from './service-B';
 
 @Controller()
 export class AppController {
-  // constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {}
 
-  // @Get()
-  // getHello(): string {
-  //   return this.appService.getHello();
-  // }
-
-  constructor(private readonly serviceB: ServiceB) { }
-
-  @Get('/serviceB')
-  getHelloC(): string {
-    return this.serviceB.getHello();
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
   }
+
+  // constructor(private readonly serviceB: ServiceB) { }
+
+  // @Get('/serviceB')
+  // getHelloC(): string {
+  //   return this.serviceB.getHello();
+  // }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,12 @@ import { ApiController, ApiController2 } from './api/api.controller';
 import { BaseService } from './base-service';
 import { ServiceA } from './service-A';
 import { ServiceB } from './service-B';
-import { EmailService } from './email/email.service';
-import { UsersService } from './users/users.service';
-import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
+import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [],
-  controllers: [ApiController2, ApiController, AppController, UsersController],
-  providers: [AppService, BaseService, ServiceA, ServiceB, UsersService, EmailService],
+  imports: [UsersModule, EmailModule],
+  controllers: [ApiController2, ApiController, AppController],
+  providers: [AppService, BaseService, ServiceA, ServiceB],
 })
 export class AppModule {}
